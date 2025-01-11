511ddccff5041d03bc83df13cfa13048c854b469
svein-salazar <>
2025-01-11 16:05:55 -0600

    exercise 5 completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcomeMsg();
+
+        string Name = PromptName();
+        int favoriteNumber = PromptNumber();
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(Name, squaredNumber);
+    }
+
+    static void DisplayWelcomeMsg()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+    static string PromptName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
